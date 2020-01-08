0f657f1eb69e481b50be448d8e521a264f061844
Carl Ehrett <cehrett@gmail.com>
2020-01-08 11:15:21 -0500

    Cleaning up the distro

--- CTO_dual_calib/dcto_paper_figures.m
+++ CTO_dual_calib/dcto_paper_figures.m
@@ -17,6 +17,44 @@ addpath([dpath,'stored_data']);
 addpath([dpath,'Example']);
 addpath([dpath,'Example\Ex_results']);
 
+%% Get example computer model output
+clc ; clearvars -except dpath ; close all ; 
+
+fig=figure('Position',[10 10 400 400]);
+
+% Define inputs
+xmin = .5;
+xrange = .5;
+x = linspace(0,1);
+t1min = 1.5;
+t1range = 3;
+t1=linspace(0,1);
+t2min = 0;
+t2range = 5;
+t2 = linspace(0,1);
+
+[X,T1,T2] = meshgrid(x,t1,t2) ; 
+Y = reshape(dual_calib_example_fn(X(:),xmin,xrange,T1(:),t1min,t1range,...
+    T2(:),t2min,t2range,0,1,0,true),length(x),length(t1),length(t2));
+
+% Take a look
+xidx=100;
+xx=reshape(X(:,xidx,:),100,100);
+tt1=reshape(T1(:,xidx,:),100,100);
+tt2=reshape(T2(:,xidx,:),100,100);
+surfax = ...
+    surf(tt1*t1range+t1min,tt2*t2range+t2min,...
+    reshape(Y(:,xidx,:),100,100),...
+    'EdgeAlpha',.4);
+xlabel('t_c');ylabel('t_d');zlabel('f(x,t_c,t_d)');
+
+fig.Children.View = [255 8];
+set(fig,'color','w');
+
+figstr = 'FIG_obj_fn';
+set(fig,'PaperPositionMode','auto')
+% print(fig,figstr,'-depsc','-r600')
+
 %% Compare DCTO and KOH+CTO posteriors for theta1,theta2 (with prior)
 clc ; clearvars -except dpath ; close all ;
 
@@ -70,8 +108,8 @@ plot([theta1 theta1],get(gca,'YLim'),'--','Color',[.85 .85 0],...
     'LineWidth',1.5);
 % Put a legend on it
 lg1 = legend('Prior dist.','KOH','DCTO','True value');
-title('Prior and posterior distributions of \theta_1');
-xlabel('\theta_1');
+title('Prior and posterior distributions of \theta_c');
+xlabel('\theta_c');
 yticks([]);
 set(f1,'color','white');
 
@@ -93,9 +131,9 @@ theta2 = results{discrep+1,1}.true_theta2;
 plot([theta2 theta2],get(gca,'YLim'),'--','Color',[.85 .85 0],...
     'LineWidth',1.5);
 % Put a legend on it
-lg1 = legend('Prior dist.','KOH','DCTO','Optimum');
-title('Prior and posterior distributions of \theta_2');
-xlabel('\theta_2');
+lg2 = legend('Prior dist.','CTO','DCTO','Optimum');
+title('Prior and posterior distributions of \theta_d');
+xlabel('\theta_d');
 yticks([]);
 set(f2,'color','white');
 
@@ -172,7 +210,7 @@ plot([theta1 theta1],get(gca,'YLim'),'--','Color',[.85 .85 0],...
 % Put a legend on it
 lg1 = legend('Prior dist.','SFD','AS','True value');
 % title('Prior and posterior distributions of \theta_1');
-xlabel('\theta_1');
+xlabel('\theta_c');
 yticks([]);
 set(f1,'color','white');
 flushLegend(lg1,'ne');
@@ -197,7 +235,7 @@ plot([theta2 theta2],get(gca,'YLim'),'--','Color',[.85 .85 0],...
 % Put a legend on it
 lg2 = legend('Prior dist.','SFD','AS','Optimum');
 % title('Prior and posterior distributions of \theta_2');
-xlabel('\theta_2');
+xlabel('\theta_d');
 yticks([]);
 set(f2,'color','white');
 flushLegend(lg2,'ne');
@@ -208,6 +246,6 @@ figstr1 = ['FIG_dual_calib_SDOE_comp_theta1-d',int2str(discrep)];
 figstr2 = ['FIG_dual_calib_SDOE_comp_theta2-d',int2str(discrep)];
 set(f1,'PaperPositionMode','auto')
 set(f2,'PaperPositionMode','auto')
-% print(f1,figstr1,'-depsc','-r600')
-% print(f2,figstr2,'-depsc','-r600')
+print(f1,figstr1,'-depsc','-r600')
+print(f2,figstr2,'-depsc','-r600')
 end % end of for loop used to run this section for multiple discreps
